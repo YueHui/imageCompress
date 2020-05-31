3dd65bc76ce1aaf5857d937f86aec93013fbc7a4
YueHui <527329923@qq.com>
2020-05-31 17:27:11 +0800

    v0.1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-const {app,BrowserWindow,ipcMain,net,globalShortcut} = require("electron");
+const {app,BrowserWindow,ipcMain,dialog,nativeImage,shell} = require("electron");
 
-let win;
+const fs = require('fs');
+const path = require('path');
+
+let win,type='dir',baseDir = '',savePath='',dataList=[];
 
 function createWindow(){
 	win = new BrowserWindow({
-		width:800,
-        height:600,
+		width:1000,
+        height:800,
+		resizable:false,
+		fullscreenable:false,
+		// titleBarStyle:'hidden',
         title:"图片压缩",
 		webPreferences: {
 			nodeIntegration: true,
@@ -16,24 +22,37 @@ function createWindow(){
 		win = null
     })
     win.loadFile('./src/index.html');
-
-    // win.on('focus', () => {
-    //     globalShortcut.register('CommandOrControl+F', function () {
-    //         if (win && win.webContents) {
-    //             win.webContents.send('on-find', '')
-    //         }
-    //     })
-    // })
-    // win.on('blur', () => {
-    //     globalShortcut.unregister('CommandOrControl+F')
-    // })
+	// win.webContents.openDevTools();
 
 }
 
-ipcMain.on("showConsole",function(){
-	if(win !== null){
-		win.webContents.openDevTools();
+ipcMain.on("chooseDir",function(event){
+	const result = dialog.showOpenDialogSync(win,{properties:['openDirectory']});
+	console.log(result);
+	if(result){
+		type = "dir";
+		dataList = result;
 	}
+	event.returnValue = result;
+})
+
+ipcMain.on("compress",function (e) {
+	if(type == "dir"){
+		console.log(dataList)
+		baseDir = dataList[0];
+		savePath = path.resolve(baseDir,'../压缩图片_'+path.basename(baseDir));
+		compressDir(fs.readdirSync(baseDir),baseDir);
+	}else{
+		dataList.forEach(file=>{
+			compress(file);
+		})
+	}
+	// console.log("complete")
+	e.reply("complete");
+})
+
+ipcMain.on("showResult",function () {
+	shell.openPath(savePath);
 })
 
 app.on('ready', createWindow);
@@ -54,3 +73,51 @@ app.on('activate', () => {
 		createWindow()
 	}
 })
+
+/**
+ * 压缩图片
+ * @param file 文件路径
+ * @param cover 是否覆盖
+ */
+function compress(file,cover=false){
+	const image = nativeImage.createFromPath(file);
+	const imageData = image.toJPEG(50);
+
+	if(cover){
+		fs.writeFileSync(file,imageData);
+		console.log('large');
+		check(file);
+	}else{
+		const relativePath = path.relative(baseDir,file);
+		const filePath = savePath+"/"+relativePath;
+		const basePath = path.dirname(filePath);
+		if(!fs.existsSync(basePath)){
+			fs.mkdirSync(basePath,{recursive:true});
+		}
+		fs.writeFileSync(filePath,imageData);
+		check(filePath);
+	}
+
+
+}
+
+function compressDir(dirList,parent){
+	dirList.forEach(file=>{
+		const path = parent+"/"+file;
+		if(file.indexOf('.')<0){
+			compressDir(fs.readdirSync(path),path);
+		}else{
+			compress(path);
+		}
+	})
+}
+
+/**
+ * 检查
+ */
+function check(file){
+	const stat = fs.statSync(`${file}`);
+	if(stat.size/1000/1000 >= 1){
+		compress(file,true);
+	}
+}
